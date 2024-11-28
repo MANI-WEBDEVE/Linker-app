95c833296879499b8c7aa0ef76a6c8539348d0e2
Muhammad Inam <nudmaufon@gmail.com>
2024-11-28 23:55:26 +0500

    all-done

--- src/app/[url]/page.tsx
+++ src/app/[url]/page.tsx
@@ -1,12 +1,28 @@
+import clientPromise from "../../lib/dbConnect";
+import { redirect } from "next/navigation";
+import { Types } from "mongoose";
 
-
-
+interface UrlData {
+    _id: Types.ObjectId;
+    url: string;
+    shortUrl: string;
+}
 
 export default async function Page({
     params,
-  }: {
-    params: Promise<{ url: string }>
-  }) {
-    const url = (await params).url
-    return <div>My Post: {url}</div>
-  }
+}: {
+    params: { url: string }
+}) {
+
+    const client = await clientPromise;
+    const url = params.url;
+    const db = client.db("Linker-app-shortner-url");
+    const collection = db.collection<UrlData>("url");
+
+    const urlAlready = await collection.findOne({ shortUrl: url });
+    if (urlAlready) {
+       
+        redirect(urlAlready.url);
+    }
+    return <div>URL not found</div>;
+}
